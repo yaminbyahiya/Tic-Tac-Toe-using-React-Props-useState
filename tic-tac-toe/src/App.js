4d8778fe2508812f34dfcbfc6bab6b2d32cce6dd
Yamin Bin Yahiya <yaminbyahiya@gmail.com>
2023-06-26 22:55:19 +0600

    Interactive Component using useState

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,27 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function Square({value}){
-  return <button className="square">{value}</button>
+function Square(){
+  const [value, setValue] = useState(null);
+  function handleClick(){
+    console.log('Clicked!');
+    setValue('X');
+  }
+  return <button className="square" onClick={handleClick}>{value}</button>
 }
 
 function App() {
   return (
     <>
       <div className='board-row'>
-        <Square value='1'></Square>
-        <Square value='2'></Square>
-        <Square value='3'></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
       </div>
       <div className='board-row'>
-        <Square value='4'></Square>
-        <Square value='5'></Square>
-        <Square value='6'></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
       </div>
       <div className='board-row'>
-        <Square value='7'></Square>
-        <Square value='8'></Square>
-        <Square value='9'></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
       </div>
     </>
   )
