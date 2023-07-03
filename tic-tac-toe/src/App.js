a12f32a6e1219ec048c17d69c9e8080b1a83a49b
Yamin Bin Yahiya <yaminbyahiya@gmail.com>
2023-07-03 17:37:04 +0600

    Logical Functionality through Immutability

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -12,11 +12,20 @@ function Square({value, onSquereClick}){
 }
 
 function App() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
   function handleClick(i){
+    if(squares[i]){
+      return;
+    }
     const nextSquares = squares.slice();
-    nextSquares[i]="X";
+    if(xIsNext){
+      nextSquares[i]="X";
+    }else{
+      nextSquares[i]="O";
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
   return (
     <>
