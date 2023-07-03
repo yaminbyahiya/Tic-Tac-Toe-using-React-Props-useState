a28bec8212a7d12150592458ee1b7fec8eba34d9
Yamin Bin Yahiya <yaminbyahiya@gmail.com>
2023-07-03 16:02:03 +0600

    State Lifting

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -2,32 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
-function Square(){
-  const [value, setValue] = useState(null);
-  function handleClick(){
-    console.log('Clicked!');
-    setValue('X');
-  }
-  return <button className="square" onClick={handleClick}>{value}</button>
+function Square({value, onSquereClick}){
+  // const [value, setValue] = useState(null);
+  // function handleClick(){
+  //   console.log('Clicked!');
+  //   setValue('X');
+  // }
+  return <button className="square" onClick={onSquereClick}>{value}</button>
 }
 
 function App() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  function handleClick(i){
+    const nextSquares = squares.slice();
+    nextSquares[i]="X";
+    setSquares(nextSquares);
+  }
   return (
     <>
       <div className='board-row'>
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value={squares[0]} onSquereClick={() => handleClick(0)}></Square>
+        <Square value={squares[1]} onSquereClick={() => handleClick(1)}></Square>
+        <Square value={squares[2]} onSquereClick={() => handleClick(2)}></Square>
       </div>
       <div className='board-row'>
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value={squares[3]} onSquereClick={() => handleClick(3)}></Square>
+        <Square value={squares[4]} onSquereClick={() => handleClick(4)}></Square>
+        <Square value={squares[5]} onSquereClick={() => handleClick(5)}></Square>
       </div>
       <div className='board-row'>
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value={squares[6]} onSquereClick={() => handleClick(6)}></Square>
+        <Square value={squares[7]} onSquereClick={() => handleClick(7)}></Square>
+        <Square value={squares[8]} onSquereClick={() => handleClick(8)}></Square>
       </div>
     </>
   )
