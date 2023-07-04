0b4305b0a9e618ad97914a18691c9b9a7a127ffc
Yamin Bin Yahiya <yaminbyahiya@gmail.com>
2023-07-04 16:18:52 +0600

    Move History Functionality using Array Mapping

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -14,11 +14,33 @@ function Square({value, onSquereClick}){
 function App() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
   function handlePlay(nextSquares){
-    setHistory([...history, nextSquares]);
+    const nextHistory = [...history.slice(0, currentMove+1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length-1);
     setXIsNext(!xIsNext);
   }
+
+  function jumpTo(nextMove){
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
 
   return(
     <div className='game'>
@@ -27,7 +49,7 @@ function App() {
       </div>
       <div className='game-info'>
         <ol>
-
+          {moves}
         </ol>
       </div>
     </div>
